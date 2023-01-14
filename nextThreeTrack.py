a014f02aa55469202863c2a0950d50a49d7f9c56
JunayedIslam1 <junayedjohny@gmail.com>
2023-01-15 02:47:56 +0600

    nextThreeLine Done

--- nextThreeTrack.py
+++ nextThreeTrack.py
@@ -1,12 +1,13 @@
 import pygame
 pygame.init()
 window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('track4.png')
+track = pygame.image.load('track5.png')
 car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 155
 car_y = 280
 cam_x_offset = 0
+cam_y_offset = 0
 focal_dist = 25
 direction = 'up'
 drive = True
@@ -18,20 +19,40 @@ while drive:
             drive = False
 # detect road
     cam_x = car_x + cam_x_offset + 15
-    cam_y = car_y + 15
+    cam_y = car_y + cam_y_offset + 15
     up_px = window.get_at((cam_x, cam_y - focal_dist))[0]
+    down_px = window.get_at((cam_x, cam_y + focal_dist))[0]
     right_px = window.get_at((cam_x + focal_dist, cam_y))[0]
-    print(up_px, right_px)
+    print(up_px, right_px, down_px)
 # change-direction for take turn
     if direction == 'up' and up_px != 255 and right_px == 255:
         direction = 'right'
         cam_x_offset = 30
         car = pygame.transform.rotate(car, -90)
+    elif direction == 'right' and right_px != 255 and down_px == 255:
+        direction= 'down'
+        car_x = car_x + 30
+        cam_x_offset = 0
+        cam_y_offset = 30
+        car = pygame.transform.rotate(car, -90)
+    elif direction == 'down' and down_px != 255 and right_px == 255:
+        direction = 'right'
+        car_y = car_y + 30
+        cam_y_offset = 0
+        cam_x_offset = 30
+        car = pygame.transform.rotate(car, 90)
+    elif direction == 'right' and right_px != 255 and up_px == 255:
+        direction = 'up'
+        car_x = car_x + 30
+        cam_x_offset = 0
+        car = pygame.transform.rotate(car, 90)
 # drive
     if direction== 'up' and up_px == 255:
         car_y = car_y - 2
-    if direction == 'right' and right_px == 255:
+    elif direction == 'right' and right_px == 255:
         car_x = car_x + 2
+    elif direction == 'down' and down_px == 255:
+        car_y = car_y + 2
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
     pygame.draw.circle(window, (0, 255, 0), (cam_x, cam_y), 5, 5)
